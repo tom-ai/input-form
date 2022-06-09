bf55bf438470b62efa651d014a1e566cc9e8b6a6
Tom Harris <tomharris3@me.com>
2022-06-09 15:12:14 +0100

    split up login and signup into separate components

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,56 +1,17 @@
 import { useState } from "react";
-import CreateAccount from "./CreateAccount";
+import Login from "./Login";
+import SignUp from "./SignUp";
 
 function Form() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-
   const [isCreating, setIsCreating] = useState(false);
   const toggleCreating = () => {
     setIsCreating((currState) => !currState);
-    console.log("yes", isCreating);
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(email, password);
-  };
-
-  return (
-    <div className="container form">
-      <form id="loginForm" onSubmit={handleSubmit}>
-        <div className="form-row">
-          <label for="email">Email:</label>
-          <input
-            id="email"
-            type="email"
-            name="email"
-            onChange={(event) => setEmail(event.target.value)}
-          />
-        </div>
-        <div className="form-row">
-          <label for="password">Password:</label>
-          <input
-            id="password"
-            type="password"
-            name="password"
-            onChange={(event) => setPassword(event.target.value)}
-          />
-        </div>
-        <CreateAccount
-          isCreating={isCreating}
-          toggleCreating={toggleCreating}
-        />
-        <button className="form-row button" type="submit">
-            {isCreating ? "Create account" : "Submit"}
-        </button>
-        {isCreating && (
-          <a className="form-row" href="#" onClick={toggleCreating}>
-            I have an account already!
-          </a>
-        )}
-      </form>
-    </div>
-  );
+  if (!isCreating) {
+    return <Login isCreating={isCreating} toggleCreating={toggleCreating} />;
+  } else {
+    return <SignUp isCreating={isCreating} toggleCreating={toggleCreating} />;
+  }
 }
 export default Form;
